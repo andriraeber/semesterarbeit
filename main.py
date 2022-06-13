062b465cca5ea1392a9af6f8920904c7436c3425
araber <73129728+andriraeber@users.noreply.github.com>
2022-06-13 21:56:58 +0200

    Push 9, Kalorienrechner erste rechnungen und class Calories

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class User:  # Definition eines User, ein User muss die Atribute: ID, Username, 
                           sort_keys=True, indent=4)
 
 
-def jsonToUser():  # ?
+def jsonToUser():  # erstellt Liste aus Json Datei (user)
     usersfile = open("data/users.json")
     usersjson = json.load(usersfile)
     usersfile.close()
@@ -40,7 +40,7 @@ def jsonToUser():  # ?
     for item in usersjson["Users"]:  # Speichert neuen User in Json Datei ab für spätere Wiederverwendung
         users.append(User(id=item["id"], username=item["username"], password=item["password"],
                           targetWeight=item["targetWeight"]))
-    return users
+    return users  # ? gibt User zurück
 
 
 @app.before_request
@@ -51,12 +51,12 @@ def before_request():  # überprüfung des Benutzer, der sich einloggen möchte.
         g.user = user
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])  # Loginseite mit der wir beginnen
 def login():  # Login eines bestehenden Benutzers
     if request.method == "POST":
         if request.form.get("registrieren") == "registrieren":
             usersfile = open("data/users.json")
-            users = json.load(usersfile)  # Ist der User in der Json Datei vorhanhden?
+            users = json.load(usersfile)  # Ist der User in der Json Datei vorhanden?
             usersfile.close()  # ?
             userId = users["Users"][-1]["id"] + 1  # greiffe letzten Eintrag auf ([-1]) und rechne zu jener ID +1
             newUsername = request.form["username"]
@@ -67,13 +67,13 @@ def login():  # Login eines bestehenden Benutzers
             usersfile = open("data/users.json", "w")
             json.dump(users, usersfile)
             print(users)  # ?
-        else:  # ?
+        else:  # ? Tobi ist das wenn wir den User nicht erkennen?
             users = jsonToUser()
             session.pop("user_id", None)
             username = request.form["username"]
             password = request.form["password"]
 
-            user = [x for x in users if x.username == username][0]  # Wieso hats da eine Null?
+            user = [x for x in users if x.username == username][0]  # Wieso hat es da eine Null?
             if user and user.password == password:
                 session["user_id"] = user.id
                 return redirect(url_for("profile"))
@@ -91,16 +91,14 @@ def profile():  # Aufrufen der Profielseite falls durch Falscheingabe nicht wied
     return render_template("profile.html")
 
 
-@app.route("/kalorienrechner", methods=["GET", "POST"])
+@app.route("/kalorienrechner",
+           methods=["GET", "POST"])  # Name stimmt noch nicht, führ in die Irre. hier wird das Zielgewixht definiert.
 def erfassungzielgewicht():
     if request.method.lower() == "get":
         return render_template("erfassungzielgewicht.html")
-    if request.methode.lower() == "post":
-        name = request.form["vorname"]
-        return name
 
 
-def selectAllWeights(conn):  # gibt uns Gewichte sortiert nach Datum zurück
+def selectAllWeights(conn):  # gibt uns Gewichte sortiert nach Datum zurück. Conn = Reihe
     cur = conn.cursor()
     cur.execute("SELECT time, weight FROM weights")
     rows = cur.fetchall()
@@ -111,7 +109,7 @@ def selectAllWeights(conn):  # gibt uns Gewichte sortiert nach Datum zurück
                             key=lambda x: x[1])))  # nimmt tuples auseinander und schreibt die elemente in je eine Liste
 
 
-def createConection(dbFile):  # Was passiert da Tobi? Erzeugt eine Verlinkung mit dem dbFile
+def createConection(dbFile):  # Was passiert da Tobi? Erzeugt eine Verlinkung mit dem dbFile? Versteh ich nicht.
     conn = None  # https://www.sqlitetutorial.net/sqlite-python/creating-database/
     try:
         conn = sqlite3.connect(dbFile)
@@ -120,7 +118,8 @@ def createConection(dbFile):  # Was passiert da Tobi? Erzeugt eine Verlinkung mi
     return conn
 
 
-def createTable(conn, sqlCommand):  # https://www.sqlitetutorial.net/sqlite-python/creating-database/
+def createTable(conn,
+                sqlCommand):  # https://www.sqlitetutorial.net/sqlite-python/creating-database/,  # Was passiert da Tobi? Erzeugt eine Verlinkung mit dem dbFile? Versteh ich nicht.
     try:
         c = conn.cursor()
         c.execute(sqlCommand)
@@ -128,33 +127,35 @@ def createTable(conn, sqlCommand):  # https://www.sqlitetutorial.net/sqlite-pyth
         print(e)
 
 
-def insertWeight(conn, weight):
+def insertWeight(conn, weight):  # Was passiert da Tobi? Erzeugt eine Verlinkung mit dem dbFile? Versteh ich nicht.
     sql = "INSERT INTO weights(time,weight) VALUES(?,?)"
     cur = conn.cursor()
-    cur.execute(sql, weight)
+    cur.execute(sql, weight)  # sql wie funktioniert dies genau?
     conn.commit()
     return cur.lastrowid
 
 
-@app.route("/filterData", methods=["GET", "POST"])
+@app.route("/filterData", methods=["GET", "POST"])  #
 def filterData():
-    filter = request.form["zeitspanne"][0]
+    filter = request.form["zeitspanne"][0]  # defienren des Filters, aus dem Form Zeitspanne. Stimmt das Tobi?
     conn = createConection("data/gewichtuser.db")
     weights = selectAllWeights(conn)
-    switcher = {
+    switcher = {  # Definition des Switchers(Auswahlfilter) inklusive Rechnung.
         "H": date.today(),
         "7": date.today() - timedelta(days=7),
         "4": date.today() - timedelta(days=30),
         "6": date.today() - timedelta(days=182),
         "1": date.today() - timedelta(days=365)
     }
-    filteredWeights = [[], []]
+    filteredWeights = [[], []]  # Filter in der zewi Listen in der Liste
     for x in range(0, len(weights[1])):
         item = weights[1][x]
-        if datetime.strptime(item, '%Y-%m-%d %H:%M:%S').date() >= switcher[filter]:
+        if datetime.strptime(item, '%Y-%m-%d %H:%M:%S').date() >= switcher[
+            filter]:  # hinter Y m d muss man noch die Zeitangeben und das ganze als .date() definieren Tobi wie erkläre ich .date()
             filteredWeights[0].append(weights[0][x])
             filteredWeights[1].append(item)
-    div = viz(filteredWeights)
+    div = viz(
+        filteredWeights)  # was ist ein div und viz genau Tobi? Viz = visualisierung und div konnte ich nicht herausfinden.
     return render_template('fortschritt.html', name=g.user.username, viz_div=div)
 
 
@@ -163,7 +164,7 @@ def viz(data):
     fig.update_traces(textposition="bottom right")
     users = jsonToUser()
     targetWeight = [x for x in users if x.username == g.user.username][0].targetWeight
-    fig.add_trace(
+    fig.add_trace(  # fühgt in fig ein....
         go.Scatter(
             x=[data[1][0], data[1][-1]],
             # dubble linked zwei listen wenn wir bei -1 sind sind wir beim letzten Element (-1 holt uns letztes ELement)
@@ -173,7 +174,7 @@ def viz(data):
             showlegend=False)
     )
 
-    div = plot(fig, output_type="div")
+    div = plot(fig, output_type="div")  # drückt fig Tabelle aus.
     return div
 
 
@@ -199,69 +200,116 @@ def fortschritt():  # erstellen einer neuen Table , falls noch keine vorhanden u
     return render_template('fortschritt.html', name=g.user.username, viz_div=div)
 
 
-@app.route("/ernaehrung")
+@app.route("/ernaehrung", methods=["GET", "POST"])
 def ernaehrung():
-    if request.method.lower() == "get":
-        return render_template("ernaehrung.html")
+    if request.method == "POST":
+        calories = Calories(
+            session["user_id"],
+            int(request.form['age']),
+            request.form['gender'],
+            int(request.form['weight']),
+            int(request.form['height']),
+            int(request.form["activity"]),
+        )
+        caloriesfile = open("data/calories.json")
+        caloriesjson = json.load(caloriesfile) or []
+        caloriesfile.close()
+        caloriesjson.append(calories.toJSON())
+        caloriesfile = open("data/calories.json", "wt")
+        json.dump(caloriesjson, caloriesfile)
+        caloriesfile.close()
+        return render_template("ernaehrung.html", **calories.toJSON())
+    return render_template(
+        'ernaehrung.html',
+    )
 
 
-"""
-    def user_info(): # Anfang Einkopierter Code
-    age = int(input('What is your age: '))
-    gender = input('What is your gender: ')
-    weight = int(input('What is your weight: '))
-    height = int(input('What is your height in inches: '))
 
-    if gender == 'male':
-        c1 = 66
-        hm = 6.2 * height
-        wm = 12.7 * weight
-        am = 6.76 * age
-    elif gender == 'female':
-        c1 = 655.1
-        hm = 4.35 * height
-        wm = 4.7 * weight
-        am = 4.7 * age
+class Calories:  # Definition einer Calorie eingabe, ein Calorieeingabe muss die Atribute: Age, Gender, Weight und height haben.
+    def __init__(self, user_id, age, gender, weight, height, activity_level):
+        self.user_id = user_id
+        self.age = age
+        self.gender = gender
+        self.weight = weight
+        self.height = height
+        self.bmr = self.get_Bmr()
+        self.activity_level = activity_level
+        self.calculated_activity = self.calculate_activity()
 
-    #BMR = 665 + (9.6 X 69) + (1.8 x 178) – (4.7 x 27)
-    bmr_result = c1 + hm + wm - am
-    return(int(bmr_result))
+    def __repr__(self):  # ?
+        return f"<User: {self.age}>"
 
-def calculate_activity(bmr_result):
-    activity_level = input('What is your activity level (none, light, moderate, heavy, or extreme): ')
+    def toJSON(self):  # Speichert neuen User in Json Datei ab, beginnend mit Indent 4 aufsteigend.
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
 
-    if activity_level == 'none':
-        activity_level = 1.2 * bmr_result
-    elif activity_level == 'light':
-        activity_level = 1.375 * bmr_result
-    elif activity_level == 'moderate':
-        activity_level = 1.55 * bmr_result
-    elif activity_level == 'heavy':
-        activity_level = 1.725 * bmr_result
-    elif activity_level == 'extreme':
-        activity_level = 1.9 * bmr_result
+    def get_Bmr(self):
+        if self.gender == 'male':
+            c1 = 66
+            hm = 6.2 * self.height
+            wm = 12.7 * self.weight
+            am = 6.76 * self.age
+        elif self.gender == 'female':
+            c1 = 655.1
+            hm = 4.35 * self.height
+            wm = 4.7 * self.weight
+            am = 4.7 * self.age
+        else:
+            return None
+        # BMR = 665 + (9.6 X 69) + (1.8 x 178) – (4.7 x 27)
+        return c1 + hm + wm - am
 
-    return(int(activity_level))
+    def calculate_activity(self):
+        activity_level = self.activity_level
+        bmr_result = self.bmr
+        if activity_level == 'none':
+            activity_level = 1.2 * bmr_result
+        elif activity_level == 'light':
+            activity_level = 1.375 * bmr_result
+        elif activity_level == 'moderate':
+            activity_level = 1.55 * bmr_result
+        elif activity_level == 'heavy':
+            activity_level = 1.725 * bmr_result
+        elif activity_level == 'extreme':
+            activity_level = 1.9 * bmr_result
 
-def gain_or_lose(activity_level):
-    goals = input('Do you want to lose, maintain, or gain weight: ')
+        return (int(activity_level))
 
-    if goals == 'lose':
-        calories = activity_level - 500
-    elif goals == 'maintain':
-        calories = activity_level
-    elif goals == 'gain':
-        gain = int(input('Gain 1 or 2 pounds per week? Enter 1 or 2: '))
-        if gain == 1:
-            calories = activity_level + 500
-        elif gain == 2:
-            calories = activity_level + 1000
+    def gain_or_lose(activity_level):
+        goals = input('Do you want to lose, maintain, or gain weight: ')
 
-    print('in order to ', goals, 'weight, your daily caloric goals should be', int(calories), '!')
+        if goals == 'lose':
+            calories = activity_level - 500
+        elif goals == 'maintain':
+            calories = activity_level
+        elif goals == 'gain':
+            gain = int(input('Gain 1 or 2 pounds per week? Enter 1 or 2: '))
+            if gain == 1:
+                calories = activity_level + 500
+            elif gain == 2:
+                calories = activity_level + 1000
+
+        print('in order to ', goals, 'weight, your daily caloric goals should be', int(calories), '!')
 
 
-gain_or_lose(calculate_activity(user_info())) # Ende Einkopierter Code
-"""
+def jsonToCalories():  # erstellt Liste aus Json Datei (calories)
+    caloriesfile = open("data/calories.json")
+    caloriesjson = json.load(caloriesfile)
+    caloriesfile.close()
+    calories = []
+
+    for item in caloriesjson["Calories"]:  # Speichert neuen User in Json Datei ab für spätere Wiederverwendung
+        calories.append(Calories(
+            usere_ide=item["user_id"],
+            age=item["Age"],
+            gender=item["Geschlecht"],
+            weight=item["Gewicht"],
+            height=item["Grösse"],
+            activity_level=item["activity_level"])
+        )
+    return calories  # ? gibt Calories zurück
+
+
 
 if __name__ == "__main__":  # Verlinkung zur URL /500
     app.run(debug=True, port=5000)
